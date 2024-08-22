226833a2801e227fc098c10fe51d1b8ea14a2b06
Debayan Biswas <dbiswas2001@gmail.com>
2024-08-23 01:20:26 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
+import Header from "./Components/Header/Header.jsx"
+import VideoSection from "./Components/VideoSection/VideoSection.jsx"
+import DesignImagesSection from "./Section/DesignImages/DesignImages.jsx"
+
+import MotionGraphics from "./Assets/Video/motiongraphics.mp4"
+import graphicsdesign1 from "./Assets/Image/graphicsdesign1.jpg"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+        <style>
+        @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@1,500&display=swap');
+        </style>
+        <Header/>
+        <div>
+            <VideoSection src={MotionGraphics}/>
+            <div style={{}}>
+              <DesignImagesSection/>
+            </div>
+            
+          
+        </div>
+
+        <hr style={{margin: "50px 200px 50px 200px"}}/>
+        
+        
     </div>
+    
   );
 }
 
